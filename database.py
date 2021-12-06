b8610c7126d17f874f92c15c92b3f1e048b36080
GladValakas31 <95177254+GladValakas31@users.noreply.github.com>
2021-12-06 21:13:52 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import psycopg2
 from psycopg2 import OperationalError
+from config import DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT
 
 
 def create_connection(db_name, db_user, db_password, db_host, db_port):
@@ -32,7 +33,7 @@ def execute_query(query):
     :param query: запрос в PostrgeSQL
     :return: None
     """
-    connection = create_connection("students", "", "", "", "") # введите свои значения для подключения к БД
+    connection = create_connection(DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT)
     cursor = connection.cursor()
     try:
         cursor.execute(query)
@@ -50,7 +51,7 @@ def check_for_presence_in_db(stud):
     :return: True, если студент есть в БД и False, если его там нет или если БД пустая
     """
 
-    connection = create_connection("students", "", "", "", "") # введите свои значения для подключения к БД
+    connection = create_connection(DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT)
     cursor = connection.cursor()
     result = None
     try:
@@ -82,7 +83,7 @@ def data_recording(fam: str, name: str, patronymic: str, grp: str, var: str, git
 
     """Функция, осуществляющая запись ученика в БД, ничего не возвращает"""
 
-    connection = create_connection("", "", "", "", "") # введите свои значения для подключения к БД
+    connection = create_connection(DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT)
     insert_query = (f"INSERT INTO students (fam, name, patronymic, grp, var, git, res) VALUES ('{fam}', '{name}', '{patronymic}', '{grp}', '{var}', '{git}', {res})")
 
     cursor = connection.cursor()
@@ -96,7 +97,7 @@ def code_check_entry(fam, name, patronymic, grp, var, git):
     Функция, проверяющая значение столбца res у определенного студента
     :return: значение столбца res (True или False)
     """
-    connection = create_connection("students", "", "", "", "") # введите свои значения для подключения к БД
+    connection = create_connection(DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT)
     cursor = connection.cursor()
     result = None
     try:
@@ -124,7 +125,7 @@ def print_res():
     Функция, осуществляющая вывод всех учеников
     :return: Возвращает всех учеников из БД
     """
-    connection = create_connection("students", "", "", "", "") # введите свои значения для подключения к БД
+    connection = create_connection(DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT)
     cursor = connection.cursor()
     result = None
     try:
