45e86d8dc2cbe09014ad44d01f6d1727c177cfcf
GladValakas31 <95177254+GladValakas31@users.noreply.github.com>
2021-12-06 15:52:57 +0300

    Update database.py

--- database.py
+++ database.py
@@ -22,37 +22,35 @@ def create_connection(db_name, db_user, db_password, db_host, db_port):
 
 
 
-def execute_query(connection, query):
+def execute_query(query):
     """
     Функция, осуществляющая введение запроса в БД через переменную query.
-
     UPDATE students
     SET res = True
     WHERE fam='...' AND name='...' AND patronymic='...' AND grp='...' AND var='...'AND git='...';
     Это запрос для обновления инф-ции в столбце res опеределенного студента
-
-    :param connection: подключение к БД
     :param query: запрос в PostrgeSQL
     :return: None
     """
-    connection.autocommit = True
+    connection = create_connection("student", "postgres", "89377366098v", "127.0.0.1", "5432")
     cursor = connection.cursor()
     try:
         cursor.execute(query)
+        connection.commit()
         print("Запрос query выполнено успешно")
     except OperationalError:
         print("Произошла ошибка 'OperationalError'")
 
 
 
-def check_for_presence_in_db(connection, stud):
+def check_for_presence_in_db(stud):
     """
     Функция, проверяющая, есть ли студент в БД.
-    :param connection: подключение к БД
-    :param stud: это пермемнная вида: 'Имя Фамилия Отчество Группа Вариант, Гитхаб'
+    :param stud: это пермемнная вида: 'Имя Фамилия Отчество Группа Вариант Гитхаб'
     :return: True, если студент есть в БД и False, если его там нет или если БД пустая
     """
 
+    connection = create_connection("student", "postgres", "89377366098v", "127.0.0.1", "5432")
     cursor = connection.cursor()
     result = None
     try:
@@ -78,11 +76,13 @@ def check_for_presence_in_db(connection, stud):
 
 
 
-def data_recording(connection, fam: str, name: str, patronymic: str, grp: str, var: str, git: str, res: bool) -> None:
+
+
+def data_recording(fam: str, name: str, patronymic: str, grp: str, var: str, git: str, res: bool) -> None:
 
     """Функция, осуществляющая запись ученика в БД, ничего не возвращает"""
 
-    create_connection("student", "postgres", "89377366098v", "127.0.0.1", "5432")
+    connection = create_connection("student", "postgres", "89377366098v", "127.0.0.1", "5432")
     insert_query = (f"INSERT INTO students (fam, name, patronymic, grp, var, git, res) VALUES ('{fam}', '{name}', '{patronymic}', '{grp}', '{var}', '{git}', {res})")
 
     cursor = connection.cursor()
@@ -90,14 +90,13 @@ def data_recording(connection, fam: str, name: str, patronymic: str, grp: str, v
     connection.commit()
 
 
-
-def code_check_entry(connection, fam, name, patronymic, grp, var, git):
+def code_check_entry(fam, name, patronymic, grp, var, git):
 
     """
     Функция, проверяющая значение столбца res у определенного студента
     :return: значение столбца res (True или False)
     """
-
+    connection = create_connection("student", "postgres", "89377366098v", "127.0.0.1", "5432")
     cursor = connection.cursor()
     result = None
     try:
@@ -120,13 +119,12 @@ def code_check_entry(connection, fam, name, patronymic, grp, var, git):
         print("Произошла ошибка OperationalError")
 
 
-
 def print_res():
     """
     Функция, осуществляющая вывод всех учеников
-    :param connection: Подключение к БД
     :return: Возвращает всех учеников из БД
     """
+    connection = create_connection("student", "postgres", "89377366098v", "127.0.0.1", "5432")
     cursor = connection.cursor()
     result = None
     try:
@@ -148,4 +146,3 @@ def print_res():
         return result
     except OperationalError:
         print("Произошла ошибка OperationalError")
-
